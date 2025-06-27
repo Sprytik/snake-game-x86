9f93ef8f9be945f3ba29e966111d9c5822cb59cc
Maria Polishchuk <polichyk6@gmail.com>
2025-06-27 10:45:28 +0300

    add file header to snake_main.asm

---
+++ snake_main.asm
@@ -0,0 +1,14 @@
+;File: snake_main.asm
+;Descrition: 
+;    "Simple snake game inspired by old nokia phone version from early days
+;    Snake continuosly moves in one direction until player changes it by
+;    pressing one of the change direction buttons. Ends of the screen represent
+;    wall from hitting which snakes die and game ends with loss. Snake can eat
+;    food that randomly spawns on the screen which adds to her length. 
+;    When snake reaches 10 pixels long, game ends with win.
+;Author: Mariia Polishchuk 
+;Date: 2025-06-27
+;Dependencies to translate: MASM.EXE, LINK.EXE 
+;Dependencies to run: DOSBOX 
+
+
